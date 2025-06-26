f9b020a643aaa1be36c447bc162319e6fdd0770e
whuang214 <41177162+whuang214@users.noreply.github.com>
2025-06-26 11:06:43 -0400

    update import

--- app/api/isc.py
+++ app/api/isc.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
+from app.core.isc_converter import convert_text_to_isc
 import os
 
 router = APIRouter()
 
+# isc/ro
 @router.get("/")
 async def root():
     # print("OPENROUTER_API_KEY:", os.getenv("OPENROUTER_API_KEY"))
